564cde3d827821239938ee6540ce651bc858b0c3
Bruno Aguirre <yo@brunoaguirre.com>
2012-02-14 10:47:31 -0200

    Remove identity map, for now

--- test/spirit.rb
+++ test/spirit.rb
@@ -56,8 +56,6 @@ test "model relations" do
   user = User.create(name: 'Barney', room: Room.create(number: 1))
   assert user.room.is_a?(Room)
   assert_equal 1, user.room.number
-  #assert_equal user, user.room.owner
-  #assert_equal user.object_id, user.room.owner.object_id
 end
 
 test "has_many" do
